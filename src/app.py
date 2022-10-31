1df30798dc4ad24151060bc04585b6568f64bdf4
ginohmk <michotall95@gmail.com>
2022-10-31 21:03:01 +0100

    Add local host origin

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
     setup_db(app)
 
     # Initializa Cors
-    CORS(app, resources={r"*": {"origins": "*"}})
+    CORS(app, resources={r"*": {"origins": "http://localhost:3000"}})
     # CORS(app)
 
     #   After Request
